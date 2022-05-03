e94fbda36ba439846be1590666917eee6d7efae1
Taeho Kim <xogh8025@gmail.com>
2022-05-03 12:37:07 +0900

    Fix: fix rounededRectangle not complement cornerRadius error

--- Laundry-tips/Laundry-tips/Source/User Interface/LaundrySymbols/SymbolBox.swift
+++ Laundry-tips/Laundry-tips/Source/User Interface/LaundrySymbols/SymbolBox.swift
@@ -11,18 +11,22 @@ struct SymbolBox: View {
     @State var boxTitle: String
     
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading,spacing: 5){
             Text(boxTitle)
                 .font(.system(size: 24, weight: .medium))
                 .foregroundColor(.lightBlack)
             ScrollView(.horizontal, showsIndicators: false){
                 HStack {
                     ForEach(0..<10) { index in
-                        RoundedRectangle(cornerRadius: 50, style: .continuous)
-                            .fill(Color.white)
-                            .border(Color.darkGray,width: 1)
-                            .frame(width: 140, height: 140)
-                            .overlay(Text("content_\(index)").foregroundColor(.lightBlack))
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.white)
+                                .frame(width: 145, height: 145)
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.darkGray, lineWidth: 1)
+                                .frame(width: 140, height: 140)
+                                .overlay(Text("content_\(index)").foregroundColor(.lightBlack))
+                        }
                     }
                 }//HStack
             }//ScrollView_horizontal
