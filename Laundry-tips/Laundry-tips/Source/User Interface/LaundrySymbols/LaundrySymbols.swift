12df11a27ff181c625aae45146a2443e77713451
Taeho Kim <xogh8025@gmail.com>
2022-05-03 17:35:28 +0900

    Feat: make LaundrySymbols View

--- Laundry-tips/Laundry-tips/Source/User Interface/LaundrySymbols/LaundrySymbols.swift
+++ Laundry-tips/Laundry-tips/Source/User Interface/LaundrySymbols/LaundrySymbols.swift
@@ -8,16 +8,21 @@
 import SwiftUI
 
 struct LaundrySymbols: View {
+    var LaundryDescription: [[String]] = [["물 온도 95도롤 세탁", "세탁기, 손세탁 가능", "세제 종류 제한 없음", "삶을 수 있음"],["물 온도 60도로 세탁", "세탁기, 손세탁 가능", "세제 종류 제한 없음"],["물 온도 40도로 세탁","세탁기, 손세탁 가능", "세제 종류 제한 없음"],["물 온도 40도로 세탁", "세탁기로 약하게 세탁","약하게 손세탁 가능", "세제 종류 제한 없음"],["물 온도 30도로 세탁", "세탁기로 약하게 세탁","약하게 손세탁 가능", "중성세제 사용"],["물 온도 30도로 세탁", "세탁기 사용 불가", "약하게 손세탁", "중성세제 사용"],["물세탁 안됨"]]
+    var bleachDescription: [[String]] = [["염소계 표백제로 표백"],["염소계 표백제로 표백할 수 없음"],["산소계 표백제로 표백"],["산소계 표백제로 표백할 수 없음"],["염소.산소계 표백제로 표백"],["염소.산소계 표백제로 표백할 수 없음"]]
+    var ironingDescription: [[String]] = [["180~210도로 다림질"],["원단 위에 천을 놓고 180~210도로 다림질"],["140~160도로 다림질"],["원단 위에 천을 놓고 140~160도로 다림질"],["80~120도로 다림질"],["원단 위에 천을 놓고 80~120도로 다림질"],["다림질 할 수 없음"]]
+    var dryCleaningDescription: [[String]] = [["드라이클리닝 가능", "용제는 클로로에틸렌 또는 석유계 사용"],["드라이클리닝 가능", "용제는 석유계 가능"],["드라이클리닝 할 수 있으나 셀프 서비스는 할 수 없음(전문점에서만 가능)"],["드라이클리닝 안됨"]]
+    var dryDehydrationDescription: [[String]] = [["햇볕에 건조", "옷걸이에 걸어서 건조"],["그늘에 건조", "옷걸이에 걸어서 건조"],["햇볕에 건조", "바닥에 뉘어서 건조"],["그늘에 건조", "바닥에 뉘어서 건조"],["세탁후 건조할 때 기계 건조할 수 있음"],["세탁후 건조할 때 기계 건조할 수 없음"],["손으로 약하게 짬"],["손으로 짜면 안됨"]]
     
     var body: some View {
         ScrollView( showsIndicators: false) {
             VStack(alignment: .leading) {
                 titleBox(title: "세탁 표시 기호", subTitle: nil)
-                SymbolBox(boxTitle: "세탁")
-                SymbolBox(boxTitle: "표백제")
-                SymbolBox(boxTitle: "다림질")
-                SymbolBox(boxTitle: "드라이클리닝")
-                SymbolBox(boxTitle: "건조 및 탈수")
+                SymbolBox(symbolMenu:"Laundry", boxTitle: "세탁", number: 7, descriptionArray: LaundryDescription)
+                SymbolBox(symbolMenu:"Bleach",boxTitle: "표백제", number: 6, descriptionArray: bleachDescription)
+                SymbolBox(symbolMenu:"Ironing",boxTitle: "다림질" , number: 7 , descriptionArray: ironingDescription)
+                SymbolBox(symbolMenu:"DryCleaning",boxTitle: "드라이클리닝", number: 4, descriptionArray: dryCleaningDescription)
+                SymbolBox(symbolMenu:"DryDehydration", boxTitle: "건조 및 탈수", number: 8, descriptionArray: dryDehydrationDescription)
             }//VStack
         } //ScrollView_vertical
         .frame(width:359)
--- Laundry-tips/Laundry-tips/Source/User Interface/LaundrySymbols/SymbolBox.swift
+++ Laundry-tips/Laundry-tips/Source/User Interface/LaundrySymbols/SymbolBox.swift
@@ -8,26 +8,52 @@
 import SwiftUI
 
 struct SymbolBox: View {
+    @State var symbolMenu: String
     @State var boxTitle: String
+    @State var number: Int?
+    @State var descriptionArray: [[String]]
+    
+    private let size: CGFloat = 160
     
     var body: some View {
         VStack(alignment: .leading,spacing: 5){
             Text(boxTitle)
-                .font(.system(size: 24, weight: .medium))
+                .font(.system(size: 22, weight: .bold))
                 .foregroundColor(.lightBlack)
             ScrollView(.horizontal, showsIndicators: false){
                 HStack {
-                    ForEach(0..<10) { index in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color.white)
-                                .frame(width: 145, height: 145)
-                            RoundedRectangle(cornerRadius: 15)
-                                .stroke(Color.darkGray, lineWidth: 1)
-                                .frame(width: 140, height: 140)
-                                .overlay(Text("content_\(index)").foregroundColor(.lightBlack))
-                        }
-                    }
+                    if let number = number {
+                        ForEach(1..<number+1, id:\.self) { index in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color.white)
+                                    .frame(width: size + 5, height: size + 5)
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.darkGray, lineWidth: 1)
+                                    .frame(width: size, height: size)
+                                VStack {
+                                    VStack {
+                                        Image("\(symbolMenu)\(index)")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                        .frame(width: 3*size/7, height: 3*size/7)
+                                    }// ImageBox_VStack
+                                    .frame(height: size * 0.3)
+                                    .padding(.top, size * 0.1)
+                                    
+                                    VStack(alignment: .leading) {
+                                        ForEach(descriptionArray[index-1], id: \.self){descript in
+                                            Text(descript)
+                                        }
+                                    }
+                                    .frame(height: size * 0.4)
+                                    .font(.system(size:10))
+                                }//VStack
+                                .frame(width: size * 0.8, height: size * 0.8)
+                                    
+                            }
+                        }//ForEach
+                    }// if
                 }//HStack
             }//ScrollView_horizontal
         }
